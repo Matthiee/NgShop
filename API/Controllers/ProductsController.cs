a28aea639f5d0d5bb1ecbdbbde3e467670857299
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2020-04-13 10:20:03 +0200

    Add product and DbContext

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok("List of products");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(string id)
+        {
+            return Ok("product");
+        }
+    }
+}
---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public StoreContext(DbContextOptions<StoreContext> options) 
+            : base(options)
+        { }
+    }
+}
