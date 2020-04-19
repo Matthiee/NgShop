e5ee34156c54d3ed2a566b109d47a5918d0182b4
Matthias Beerens <3512339+Matthiee@users.noreply.github.com>
2020-04-19 20:34:02 +0200

    Add checkout module

---
+++ Client/src/app/checkout/checkout-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { CheckoutComponent } from './checkout.component';
+
+const routes: Routes = [{ path: '', component: CheckoutComponent }];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CheckoutRoutingModule {}
--- Client/src/app/routes.ts
+++ Client/src/app/routes.ts
@@ -20,6 +20,11 @@ const routes: Routes = [
     loadChildren: () => import('./basket/basket.module').then((mod) => mod.BasketModule),
     data: { breadcrumb: 'Basket' },
   },
+  {
+    path: 'checkout',
+    loadChildren: () => import('./checkout/checkout.module').then((mod) => mod.CheckoutModule),
+    data: { breadcrumb: 'Checkout' },
+  },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
